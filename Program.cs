d10ffb8e42b1317e5fd36224a6142535f2e65ebe
SaraMag25 <saramagalhaes242@gmail.com>
2025-07-09 13:34:50 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiro_proj {
+    class Program {
+        static void Main(string[] args) {
+
+            double x = 10.35784;
+            int y = 32;
+            string z = "Maria";
+            char w = 'F';
+            Console.WriteLine("Bom dia!");
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine($"{z} tem {y} ano e de dinheiro {x:F2}");
+            Console.ReadLine();
+        }
+    }
+}
