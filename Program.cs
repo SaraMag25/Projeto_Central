1bf0f2e4e70a7fe1db0ae6d87175181b8481ad35
SaraMag25 <saramagalhaes242@gmail.com>
2025-07-10 14:09:14 -0300

    Operadores aritmedicos

--- Program.cs
+++ Program.cs
@@ -5,14 +5,23 @@ namespace Primeiro_proj {
     class Program {
         static void Main(string[] args)
         {
-            float x = 4.5f;
-            double y = x;
-
-            double a;
-            float b;
-            
-            b = (float)a;
-
+            int n1 = 3 + 4 * 2;
+            int n2 = (4 + 3) * 2;
+            int n3 = 17 % 3;
+            double n4 = (double)10 / 8;
+            double n5 = 10.0 / 3; // indica que é uma expresao double
+            Console.WriteLine(n3);
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+           
+            //float x = 4.5f;
+            //double y = x;
+            //double a;
+            //float b;
+            //b = (float)a;
             //double x = 10.35784;
             //int y = 32;
             //string z = "Maria";
