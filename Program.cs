2b5065f2ac87d42ad7674436406f0ab4edeecf8e
SaraMag25 <saramagalhaes242@gmail.com>
2025-07-10 13:45:18 -0300

    Conversão implícita e casting

--- Program.cs
+++ Program.cs
@@ -3,17 +3,24 @@ using System.Globalization;
 
 namespace Primeiro_proj {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+            float x = 4.5f;
+            double y = x;
 
-            double x = 10.35784;
-            int y = 32;
-            string z = "Maria";
-            char w = 'F';
-            Console.WriteLine("Bom dia!");
-            Console.WriteLine(x);
-            Console.WriteLine(x.ToString("F2"));
-            Console.WriteLine($"{z} tem {y} ano e de dinheiro {x:F2}");
-            Console.ReadLine();
+            double a;
+            float b;
+            b = (float)a;
+
+            //double x = 10.35784;
+            //int y = 32;
+            //string z = "Maria";
+            //char w = 'F';
+            //Console.WriteLine("Bom dia!");
+            //Console.WriteLine(x);
+            //Console.WriteLine(x.ToString("F2"));
+            //Console.WriteLine($"{z} tem {y} ano e de dinheiro {x:F2}");
+            //Console.ReadLine();
         }
     }
 }
