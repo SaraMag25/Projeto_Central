c8e6032e2bf0a403ed08327f7b86a3f2be57b261
SaraMag25 <saramagalhaes242@gmail.com>
2025-07-13 21:49:57 -0300

    entrada de dados

--- Program.cs
+++ Program.cs
@@ -5,18 +5,33 @@ namespace Primeiro_proj {
     class Program {
         static void Main(string[] args)
         {
-            int n1 = 3 + 4 * 2;
-            int n2 = (4 + 3) * 2;
-            int n3 = 17 % 3;
-            double n4 = (double)10 / 8;
-            double n5 = 10.0 / 3; // indica que é uma expresao double
-            Console.WriteLine(n3);
-            double a = 1.0, b = -3.0, c = -4.0;
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
-            double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-           
+            string x;
+            int y;
+            double z;
+            char w;            
+
+            x = Console.ReadLine();
+            y = int.Parse(Console.ReadLine());
+            z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            w = char.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(w);
+
+            Console.ReadLine();
+            //int n1 = 3 + 4 * 2;
+            //int n2 = (4 + 3) * 2;
+            //int n3 = 17 % 3;
+            //double n4 = (double)10 / 8;
+            //double n5 = 10.0 / 3; // indica que é uma expresao double
+            //Console.WriteLine(n3);
+            //double a = 1.0, b = -3.0, c = -4.0;
+            //double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            //double x1 = (-b + Math.Sqrt(delta)) / 2 * a;
+            //Console.WriteLine(delta);
+            //Console.WriteLine(x1);
             //float x = 4.5f;
             //double y = x;
             //double a;
